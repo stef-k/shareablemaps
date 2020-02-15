5ab9047ee4c29a9d2393da98be78b799ba2df35e
Stef Kariotidis <stef.kariotidis@gmail.com>
2020-02-15 11:18:08 +0200

    analytics

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,7 +36,23 @@
         <meta name="twitter:title" content="Shareable Travel Maps"/>
         <meta name="twitter:description" content="Free shareable travel travel maps that load on your Google Maps application."/>
         <meta name="twitter:image" content="https://shareablemaps.com/icon-1024.jpg"/>
-        {!! Analytics::render() !!}
+
+<script> (function (i, s, o, g, r, a, m) {
+    i['GoogleAnalyticsObject'] = r;
+    i[r] = i[r] || function () {
+    (i[r].q = i[r].q || []).push(arguments)
+    }, i[r].l = 1 * new Date();
+    a = s.createElement(o), m = s.getElementsByTagName(o)[0];
+    a.async = 1;
+    a.src = g;
+    m.parentNode.insertBefore(a, m)
+})(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+ga('create', 'UA-158599443-1', {
+    'cookieDomain': 'none'
+}, 't0');
+ga('set', 'anonymizeIp', true);
+ga('send', 'pageview'); </script>
+
     </head>
 
     <body>
@@ -60,6 +76,7 @@
 <script src="https://cdn.quilljs.com/1.3.6/quill.js"></script>
 {{-- <script src="{{ asset('js/app.js') }}"></script> --}}
 <script src="{{ mix('/js/app.js') }}"></script>
+
     </body>
 
 </html>
